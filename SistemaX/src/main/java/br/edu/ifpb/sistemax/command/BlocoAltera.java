a7c1721673a875f10e848c44abfaeef3d04c09c6
jose <joseferreiravieira123@gmail.com>
2016-04-20 13:45:19 -0300

    comados com json

--- SistemaX/src/main/java/br/edu/ifpb/sistemax/command/BlocoAltera.java
+++ SistemaX/src/main/java/br/edu/ifpb/sistemax/command/BlocoAltera.java
@@ -6,8 +6,13 @@
 package br.edu.ifpb.sistemax.command;
 
 import br.edu.ifpb.sistemax.entidades.Bloco;
+import br.edu.ifpb.sistemax.entidades.Usuario;
+import br.edu.ifpb.sistemax.enuns.PapelUser;
 import br.edu.ifpb.sistemax.model.BlocoAlterarBO;
+import com.google.gson.Gson;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
@@ -22,15 +27,25 @@ public class BlocoAltera implements Command {
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) {
         try {
-            Bloco blocoOriginal = (Bloco) request.getSession().getAttribute("blogo");
-            String nome = request.getParameter("nome");
-            blocoOriginal.setNome(nome);
-            BlocoAlterarBO alterar = new BlocoAlterarBO();
-            if (alterar.alterar(blocoOriginal)) {
-                response.getWriter().print("true");
-            } else {
-                response.getWriter().print("true");
+
+            Usuario usuario = (Usuario) request.getSession().getAttribute("user");
+            Map<String, String> resultado = new HashMap<>();
+            if (usuario.getPapel() != PapelUser.Administrador || usuario.getPapel() != PapelUser.AssistenteDeSala) {
+                response.sendRedirect("pagina");
+                Bloco blocoOriginal = (Bloco) request.getSession().getAttribute("blogo");
+
+                String nome = request.getParameter("nome");
+                blocoOriginal.setNome(nome);
+                BlocoAlterarBO alterar = new BlocoAlterarBO();
+                if (alterar.alterar(blocoOriginal)) {
+                    resultado.put("resultado", "Bloco atualizado");
+                } else {
+                    resultado.put("resultado", "erro");
+                }
             }
+            String json = new Gson().toJson(resultado);
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
         } catch (IOException ex) {
             Logger.getLogger(BlocoAltera.class.getName()).log(Level.SEVERE, null, ex);
         }
--- SistemaX/src/main/java/br/edu/ifpb/sistemax/command/BlocoCadastrar.java
+++ SistemaX/src/main/java/br/edu/ifpb/sistemax/command/BlocoCadastrar.java
@@ -6,12 +6,17 @@
 package br.edu.ifpb.sistemax.command;
 
 import br.edu.ifpb.sistemax.entidades.Bloco;
+import br.edu.ifpb.sistemax.entidades.Usuario;
+import br.edu.ifpb.sistemax.enuns.PapelUser;
 import br.edu.ifpb.sistemax.model.BlocoCadastrarBO;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.gson.Gson;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -21,17 +26,28 @@ public class BlocoCadastrar implements Command {
 
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) {
+        Map<String, String> resultado = new HashMap<>();
         try {
-            String nome = request.getParameter("nome");
-            Bloco bloco = new Bloco();
-            bloco.setNome(nome);
-            BlocoCadastrarBO cadastrar = new BlocoCadastrarBO();
-            boolean result = cadastrar.cadastrar(bloco);
-            if (result) {
-                response.getWriter().print("false");
-            } else {
-                response.getWriter().print("true");
+            Usuario usuario = (Usuario) request.getSession().getAttribute("user");
+            if (usuario.getPapel() != PapelUser.Administrador || usuario.getPapel() != PapelUser.AssistenteDeSala) {
+                response.sendRedirect("pagina");
+                String nome = request.getParameter("nome");
+                if (nome.trim().isEmpty()) {
+                    resultado.put("nome", "Infome o nome do bloco");
+                }
+                Bloco bloco = new Bloco();
+                bloco.setNome(nome);
+                BlocoCadastrarBO cadastrar = new BlocoCadastrarBO();
+                boolean result = cadastrar.cadastrar(bloco);
+                if (result) {
+                    resultado.put("resultado", "bloco cadastrado");
+                }
+
             }
+
+            String json = new Gson().toJson(resultado);
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
         } catch (IOException ex) {
             Logger.getLogger(BlocoCadastrar.class.getName()).log(Level.SEVERE, null, ex);
         }
